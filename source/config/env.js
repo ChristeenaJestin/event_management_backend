b632e0edb43661c16f7503fbb7cdf05fa51f906f
ChristeenaJestin <christeenajestin7@gmail.com>
2026-06-28 12:15:40 +0530

    Initial backend setup

---
+++ source/config/env.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const env = {
+  PORT: process.env.PORT || 5000,
+  SUPABASE_URL: process.env.SUPABASE_URL,
+  SUPABASE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+};
